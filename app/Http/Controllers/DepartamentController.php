89a8377fc0525b32d6367841af89ee79a87ee96e
andres4320 <sergio.andres.garzon17@gmail.com>
2024-03-07 09:28:15 -0500

    new correction

--- app/Http/Controllers/DepartamentController.php
+++ app/Http/Controllers/DepartamentController.php
@@ -83,7 +83,7 @@ class DepartamentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id): JsonReponse
+    public function update(Request $request, $id): JsonResponse
     {
         try {
             $request->validate([
